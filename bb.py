f8133e8159b6490f1d4b0c638b3e22a74201991a
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-10 17:14:50 +0200

    Update bb.py

--- bb.py
+++ bb.py
@@ -7,9 +7,9 @@ from pyqrcode import QRCode
 col1, col2= st.beta_columns(2)
 with col1:
     st.markdown("**Saisissez le texte à convertir:**")
-    text=st.text_input("ici",key=0)
+    text=st.text_input("",key=0)
     st.markdown("**Entrez le nom de l'image à sauvegarder:**")
-    img=st.text_input("ici",key=1)
+    img=st.text_input("",key=1)
     if st.button("Générer"):
         # Adding extension as .pnf
         name=img+".png"
@@ -17,7 +17,7 @@ with col1:
         url=pyqrcode.create(text)
         # Saving QR code as  a png file
         url.show()
-        url.png(d, scale =6)
-#with col2:
-    #st.image(url.png(d, scale =6))
+        url.png(name, scale =6)
+with col2:
+    st.image(url.png(d, scale =6)
 
