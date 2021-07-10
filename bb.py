47b1f830e79e1eb85430059d373d2d908928898e
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-10 16:50:06 +0200

    Add files via upload

---
+++ bb.py
@@ -0,0 +1,22 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+
+col1, col2= st.beta_columns(2)
+with col1:
+    st.markdown("**Saisissez le texte à convertir**")
+    text=st.text_input(": ")
+    st.markdown("**Entrez le nom de l'image à sauvegarder**")
+    img=st.text_input(": "))
+    if st.button("Générer"):
+        # Adding extension as .pnf
+        name=img+".png"
+        # Creating QR code
+        url=pyqrcode.create(text)
+        # Saving QR code as  a png file
+        url.show()
+        url.png(d, scale =6)
+with col2:
+    st.image(url.png(d, scale =6))
+    
